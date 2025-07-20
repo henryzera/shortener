dcf9b87217b1f1bec21e0bc60cd9a78ab84a5756
Emanuel Henry <emanuelhenrysc@gmail.com>
2025-07-20 04:21:55 -0300

    fix: fit in the model

--- models/Link.js
+++ models/Link.js
@@ -4,7 +4,7 @@ const linkSchema = new mongoose.Schema({
     title: {type: String, required: true},
     description: String,
     url: {type: String, required: true},
-    click: Number
+    click: {type: Number, default: 0}
 });
 
 module.exports = mongoose.model('Link', linkSchema);
